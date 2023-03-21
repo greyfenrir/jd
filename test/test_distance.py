1393b9ad5517062d570624a047d445df7fbf9702
greyfenrir <grey.fenrir@gmail.com>
2023-03-21 18:47:12 +0300

    add unit tests

---
+++ test/test_distance.py
@@ -0,0 +1,33 @@
+from unittest.case import TestCase
+from distance import minimal_distance
+
+
+class TestDistance(TestCase):
+    def test_the_same(self):
+        a = 'word'
+        self.assertEqual(0, minimal_distance(a, a))
+
+    def test_empty(self):
+        a = 'word'
+        self.assertEqual(len(a), minimal_distance(a, ''))
+
+    def test_del(self):
+        a = 'word1'
+        b = 'wor'
+        self.assertEqual(2, minimal_distance(a, b))
+
+    def test_add(self):
+        a = 'word1'
+        b = 'wor'
+        self.assertEqual(2, minimal_distance(b, a))
+
+    def test_add_del(self):
+        a = 'word1'
+        b = '1word'
+        self.assertEqual(2, minimal_distance(a, b))
+
+    def test_replace(self):
+        a = 'word1'
+        b = 'word2'
+        self.assertEqual(1, minimal_distance(b, a))
+
